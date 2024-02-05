6f6b4330d739833b3ebb1d3d99aaa31ab4e19cd0
emilianogastaldo <emiliano0303@gmail.com>
2024-02-05 15:39:07 +0100

    created store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
-import AppMain from './components/AppMain.vue'
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+import AppMain from './components/AppMain.vue';
+import { store } from './data/store.js';
 export default {
   name: 'Pokedex',
   components: {
     AppMain
+  },
+  created() {
+    axios.get(endpoint).then(res => {
+      store.listPokemon = res.data.result;
+    })
   }
 };
 </script>

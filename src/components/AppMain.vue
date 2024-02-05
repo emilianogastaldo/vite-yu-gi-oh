de59daad707227735d83f4cc210dd90db84718f9
emilianogastaldo <emiliano0303@gmail.com>
2024-02-05 16:28:25 +0100

    created component pokemon card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,14 @@
 <script>
 import { store } from '../data/store.js'
+import PokemonCard from './PokemonCard.vue';
 export default {
     name: 'AppMain',
     data: () => ({
         store
     }),
+    components: {
+        PokemonCard
+    }
 }
 </script>
 
@@ -13,18 +17,7 @@ export default {
         <div class="container">
             <div class="row row-cols-lg-5 py-4 g-4 my-3">
                 <div class="col" v-for="pokemon in store.listPokemon">
-                    <div class="card p-3 text-center">
-                        <div class="border p-3 border-black rounded">
-
-                            <div class="card-img-top border border-black rounded-circle">
-                                <img :src="pokemon.imageUrl" :alt="pokemon.name" class="img-fluid rounded-circle">
-                            </div>
-                            <div class="card-body ">
-                                <h5 class="card-title"> <span>{{ pokemon.number }}</span>. {{ pokemon.name }}</h5>
-                                <p class="card-text">{{ pokemon.type1 }} {{ pokemon.type2 }}</p>
-                            </div>
-                        </div>
-                    </div>
+                    <PokemonCard :pokemon="pokemon" />
                 </div>
             </div>
         </div>
@@ -34,13 +27,5 @@ export default {
 <style lang="scss" scoped>
 main {
     background-color: aquamarine;
-}
-
-h5::after {
-    content: '';
-    display: block;
-    width: 30px;
-    border: 1px solid;
-    margin: 1rem auto;
 }
 </style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'Pokemon Card',
+    props: {
+        pokemon: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card p-3 text-center">
+        <div class="border p-3 border-black rounded">
+            <div class="card-img-top border border-black rounded-circle">
+                <img :src="pokemon.imageUrl" :alt="pokemon.name" class="img-fluid rounded-circle">
+            </div>
+            <div class="card-body ">
+                <h5 class="card-title"> <span>{{ pokemon.number }}</span>. {{ pokemon.name }}</h5>
+                <p class="card-text">{{ pokemon.type1 }} {{ pokemon.type2 }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+h5::after {
+    content: '';
+    display: block;
+    width: 30px;
+    border: 1px solid;
+    margin: 1rem auto;
+}
+</style>

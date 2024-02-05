e56404431087a11cb8262a65ceeeb5c9fb8a4d6a
emilianogastaldo <emiliano0303@gmail.com>
2024-02-05 16:23:43 +0100

    created page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,12 +10,37 @@ export default {
 
 <template>
     <main>
-        prova main
+        <div class="container">
+            <div class="row row-cols-lg-5 py-4 g-4 my-3">
+                <div class="col" v-for="pokemon in store.listPokemon">
+                    <div class="card p-3 text-center">
+                        <div class="border p-3 border-black rounded">
+
+                            <div class="card-img-top border border-black rounded-circle">
+                                <img :src="pokemon.imageUrl" :alt="pokemon.name" class="img-fluid rounded-circle">
+                            </div>
+                            <div class="card-body ">
+                                <h5 class="card-title"> <span>{{ pokemon.number }}</span>. {{ pokemon.name }}</h5>
+                                <p class="card-text">{{ pokemon.type1 }} {{ pokemon.type2 }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
 main {
     background-color: aquamarine;
+}
+
+h5::after {
+    content: '';
+    display: block;
+    width: 30px;
+    border: 1px solid;
+    margin: 1rem auto;
 }
 </style>

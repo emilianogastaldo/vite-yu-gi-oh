42ed660600b7d6561761650653cf356020f30a4b
emilianogastaldo <emiliano0303@gmail.com>
2024-02-06 15:40:58 +0100

    added types data

--- src/data/store.js
+++ src/data/store.js
@@ -2,3 +2,24 @@ import { reactive } from 'vue'
 export const store = reactive({
     listPokemon: [],
 })
+
+export const pokemonTypes = [
+    "Bug",
+    "Dark",
+    "Dragon",
+    "Electric",
+    "Fairy",
+    "Fighting",
+    "Fire",
+    "Flying",
+    "Ghost",
+    "Grass",
+    "Ground",
+    "Ice",
+    "Normal",
+    "Poison",
+    "Psychic",
+    "Rock",
+    "Steel",
+    "Water"
+]

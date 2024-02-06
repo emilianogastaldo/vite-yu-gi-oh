20922fc4e5e7eaf3693e60b2d2c72179fad41f16
emilianogastaldo <emiliano0303@gmail.com>
2024-02-06 15:48:00 +0100

    createde select

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,8 +10,9 @@ export default {
 <template>
     <div class="d-flex pt-5 justify-content-between">
         <h1 class="text-danger"> Pokedex </h1>
-        <select name="" id="">
-            <option value="">Prova</option>
+        <select name="types" id="select-type">
+            <option selected> Select a type</option>
+            <option v-for="(type, i) in types" :key="i" :value="type">{{ type }}</option>
         </select>
     </div>
 </template>

35f175b96acd38ad8e49c6f9ca71bfdb126dd6a4
emilianogastaldo <emiliano0303@gmail.com>
2024-02-06 16:18:58 +0100

    added event

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,21 @@
 <script>
 export default {
     name: 'AppHeader',
+    data: () => ({
+        typeInput: ''
+    }),
     props: {
         types: Array
-    }
+    },
+    emits: ['select-type']
 }
 </script>
 
 <template>
     <div class="d-flex pt-5 justify-content-between">
         <h1 class="text-danger"> Pokedex </h1>
-        <select name="types" id="select-type">
-            <option selected> Select a type</option>
+        <select @click="$emit('select-type')" v-model="typeInput" name="types" id="select-type">
+            <option selected disabled value="">Select a type</option>
             <option v-for="(type, i) in types" :key="i" :value="type">{{ type }}</option>
         </select>
     </div>

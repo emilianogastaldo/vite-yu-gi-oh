432a93de2144426349dcbf3cafd74db5b388d6b2
emilianogastaldo <emiliano0303@gmail.com>
2024-02-07 10:05:38 +0100

    fixed interaction

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,7 +14,7 @@ export default {
 <template>
     <div class="d-flex pt-5 justify-content-between">
         <h1 class="text-danger"> Pokedex </h1>
-        <select @click="$emit('select-type', typeInput)" v-model="typeInput" name="types" id="select-type">
+        <select @change="$emit('select-type', typeInput)" v-model="typeInput" name="types" id="select-type">
             <option selected disabled value="">Select a type</option>
             <option v-for="(type, i) in types" :key="i" :value="type">{{ type }}</option>
         </select>
